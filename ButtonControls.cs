d7ed76fb667c4f602caa02512220254d12133d9c
Christopher Chianelli <chris.chianelli@gmail.com>
2016-04-21 20:52:33 -0400

    Added Asset files for Unity Project.
    Contains all the assets required to run the first prototype.

---
+++ ButtonControls.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ButtonControls : MonoBehaviour {
+	public Button[] buttons;
+	public Image[] images;
+
+	private int selected;
+
+	// Use this for initialization
+	void Start () {
+		selected = -1;
+		for (int i = 0; i < buttons.Length; i++)
+			buttons [i].image.color = Color.cyan;
+		for (int i = 0; i < images.Length; i++)
+			images [i].color = new Color (0.9f,0.9f,0.9f,1);
+	}
+
+	public int getSelected(){
+		return selected;
+	}
+	public void changeSelected(int newSelected){
+		clearLast ();
+		selected = newSelected;
+		buttons [selected].image.color = Color.green;
+		images [selected].color = new Color (1,1,1,1);
+		buttons [selected].interactable = false;
+	}
+
+	public void clearSelected(){
+		clearLast ();
+		selected = -1;
+	}
+
+	private void clearLast()
+	{
+		if (selected != -1) {
+			buttons [selected].image.color = Color.cyan;
+			images [selected].color = new Color (0.9f,0.9f,0.9f,1);
+			buttons [selected].interactable = true;
+		}
+	}
+}
---
+++ MovePlayer.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MovePlayer : MonoBehaviour {
+
+	public Rigidbody player;
+	bool inControl = true;
+	public float speed;
+
+	// Update is called once per frame
+	void Update () {
+		if (inControl)
+		    player.velocity = new Vector3 (speed*Input.GetAxis("Horizontal"), player.velocity.y, speed*Input.GetAxis("Vertical"));
+	}
+
+	public void ChangeControl(bool newControl){
+		inControl = newControl;
+	}
+}
