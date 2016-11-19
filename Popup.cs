65f36bb396e7b3d5e7a762e2791cb8df70e6e3a3
Christopher Chianelli <chris.chianelli@gmail.com>
2016-11-19 17:06:26 -0500

    Added Popup Theatre

---
+++ Popup.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Popup : MonoBehaviour {
+	public void PopupObject(){
+		StartCoroutine (MyUtils.SmoothMove (transform, transform.position, Quaternion.Euler (0, 0, 0), 4f));
+	}
+
+	public void Reset(){
+		transform.Rotate (-45f, 0f, 0f);
+	}
+}
--- Scripts/PopupTheatre.cs
+++ Scripts/PopupTheatre.cs
@@ -3,23 +3,39 @@ using System.Collections;
 
 public class PopupTheatre : MonoBehaviour {
 	public GameObject otherStuff;
-	public GameObject myCamera;
+	public GameObject theatre;
+	public GameObject myCamera,light;
 	public Transform normalPos, popupPos, popupCameraPos;
+	public Popup[] popups;
 	private Vector3 oldCameraPos = Vector3.zero;
 	private Quaternion oldCameraRot = Quaternion.identity;
 
 	public void SetupPopupTheatre(){
 		otherStuff.SetActive (false);
+		light.SetActive (true);
 		StartCoroutine(MyUtils.SmoothMove (transform, normalPos, popupPos, 5f));
-		oldCameraPos = camera.transform.position;
-		oldCameraRot = camera.transform.rotation;
+		oldCameraPos = myCamera.transform.position;
+		oldCameraRot = myCamera.transform.rotation;
 		StartCoroutine(MyUtils.SmoothMove (myCamera.transform, popupCameraPos, 5f));
+		StartCoroutine(PopupSprites());
 	}
 
 	public void LeavePopupTheatre(){
+		foreach (Popup p in popups)
+			p.Reset ();
+		light.SetActive (false);
 		otherStuff.SetActive (true);
+		theatre.SetActive (false);
 		StartCoroutine (MyUtils.SmoothMove (transform, popupPos, normalPos, 5f));
 		StartCoroutine (MyUtils.SmoothMove (myCamera.transform, oldCameraPos, oldCameraRot, 5f));
+	}
+
+	private IEnumerator PopupSprites()
+	{
+		yield return new WaitForSeconds (5f);
+		theatre.SetActive (true);
+		foreach (Popup p in popups)
+			p.PopupObject ();
 	}
 		
 	void Update()
